24e577b0383f907112da74353b19d6ed73e337d6
juancruzsosagab <juancruzsosa@andesmar.com.ar>
2021-11-10 18:26:26 -0300

    Limpieza archivo

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
 import CartWidget from './components/CartWidget/CartWidget.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
-import Counter from './components/Counter/Counter.jsx';
-import Login from './screens/Login/Login';
 
 function App() {
   return (
@@ -12,8 +10,8 @@ function App() {
     <CartWidget/>
     </Navbar>
     <ItemListContainer />
-    <Login />
-    <Counter />
+   
+  
     </>
   );
 }
