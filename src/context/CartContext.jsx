6c5f0665b9ea0bda912e505fe0388cdd82dd7838
juancruzsosagab <juancruzsosa@andesmar.com.ar>
2021-12-21 00:03:45 -0300

    Avances

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "firebase/firestore";
+
+
 
 const CartContext = React.createContext();
 
 export function CartContextProvider({ children }) {
   const [cart, setCart] = useState([]);
+  const [OrderId, setOrderId] = useState("");
+
   let total;
 
   const calculateTotal = ()=>{
@@ -28,6 +37,19 @@ export function CartContextProvider({ children }) {
     const cartNew = cart.filter((cartItem) => cartItem.id !== item.id);
     setCart(cartNew);
   };
+
+  // Avance cración de orden, hardcodeada
+  const newOrder = {   
+    name: "juan",
+    phone: 2613733585,
+    email: "juancruz.sosag@gmail.com"
+  };
+  const db = getFirestore()
+  const ref = collection(db,"orders");
+   
+  addDoc(ref, newOrder)
+  console.log(newOrder)
+
 
   return (
     <CartContext.Provider value={{ addItem, removeItem, cart }}>
